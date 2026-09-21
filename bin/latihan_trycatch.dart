98dda5b93da0b7ee967a151c06479272f7df2d86
roypahri-coder <roypahri@gmail.com>
2026-09-21 20:16:31 +0700

    newLaV3

--- bin/latihan_trycatch.dart
+++ bin/latihan_trycatch.dart
@@ -3,8 +3,18 @@ import 'dart:convert';
 import 'package:http/http.dart'as http;
 
 void main ()async{
-  Future tarikdata() async {
-    var prabowo = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+  Future<List<dynamic>> tarikdata() async {
+    try {
+      var prabowo = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'),
+    );
+
+    var data = jsonDecode(prabowo.body);
+    return data;
+
+
+    }
+    catch(e){}
+    var prabowo = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'),
     );
 
     var data = jsonDecode(prabowo.body);
@@ -17,8 +27,9 @@ void main ()async{
    print('Data Comments');
    print('-----------------');
 
-   for (var i = 0; i < data.length; i++) {
-    print('${data[i]['name']} | ${data[i]['email']}');
-    print('-----------------');
+  //  for (var i = 0; i < data.length; i++) {
+  //   print('${element['name']} | ${element['email']}');
+  //   print('-----------------');
+
+   // data,foreach()
    }
-}
